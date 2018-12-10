60ac6506c38f15cb3ab139eff161d2c29878ba38
Tom <tom.aarnts@gmail.com>
2018-12-10 23:01:57 +0100

    finshed
    still required ful awnser generation

--- day2/main.py
+++ day2/main.py
@@ -18,7 +18,7 @@ for i in range(len(data[0])-2):
 i=0
 count2=0
 count3=0
-
+go=1
 #itterate through data
 while go==1:
     try:
@@ -37,3 +37,41 @@ while go==1:
 
 
 print('checksum=',count2*count3) #print result
+
+go=1
+k=0
+i=0
+while go==1:
+    try:
+        str=data[i]
+    except:
+        break
+
+
+    for k in range(i,len(data)-1):
+        str2=data[k]
+        miss = 0
+        hit = 0
+
+        for l in range(len(str)):
+            if str[l]==str2[l]:
+                hit=hit+1
+     
+                #print(hit)
+            else:
+                miss=miss+1
+                miss_let=l
+                #print(miss)
+                if miss>1:
+                    break
+            if l==len(str)-1 and miss==1:
+                print('string1=',str)
+                print('string2=',str2)
+                awnser2=str
+                awnser2.replace(str[miss_let],'')
+                print('awnser2=',awnser2)
+                go=0
+                break
+    i = i + 1
+
+
