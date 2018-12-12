ac2b27c8e1acb6a7b00fb4782a5ed07b5d66d19f
Tom <tom.aarnts@gmail.com>
2018-12-12 10:38:42 +0100

    5 solved and somewhat pretty

--- day5/main5.py
+++ day5/main5.py
@@ -1,4 +1,28 @@
 import numpy as np
+import string
+
+
+def mutate(str_in):
+    go=1
+    i=0
+    k=0
+
+    while go==1:
+        if i >= len(str_in) - 1 or i < 0: #if i out of bounds, reset i
+            if k == 0 and i > 0:#stop loop if no deletion
+                go = 0
+
+            #reset counters
+            i = 0
+            k = 0
+
+        if str_in[i].lower() == str_in[i + 1].lower() and str_in[i] != str_in[i + 1]:#if same letter but diffrent cap.
+            del (str_in[i:i + 2])
+            k = k + 1
+            i = i - 10
+        i=i+1
+    return len(str_in)
+
 
 #import puzzle input
 filename='data'
@@ -14,20 +38,22 @@ init=len(str)
 count=0
 
 
+awnser_1=mutate(str)
+print('awnser 1=',awnser_1)
 
-while go==1:
-    if i>=len(str)-1 or i<0:
-        if k==0 and i>0:
-            go=0
-        i = 0
-        k=0
 
-    if str[i].lower()==str[i+1].lower() and str[i]!=str[i+1]:
-        del(str[i:i+2])
-        k=k+1
-        i=i-10
-        print(i,'/',len(str),'/',init)
+alph=list(string.ascii_lowercase[:26])
+str=list(data[0])
+result=[]
 
-    i=i+1
+for letter in alph:
+    target=list(data[0])
+    target=list(filter(lambda x: x != letter, target))
+    target = list(filter(lambda x: x != letter.upper(), target))
 
-print(len(str))
+    val=mutate(target)
+    result.append(val)
+    #print(letter,'|',val)
+
+print('awnser 2=',min(result),'@',alph[result.index(min(result))])
+
